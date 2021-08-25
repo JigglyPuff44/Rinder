af062769928a13ad61c7c71ed52c904e5f55e0e2
Emma Czech <emcatherineczech@gmail.com>
2021-08-25 13:04:18 -0600

    added googleplaces API logic to homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,14 @@
+import { networkInterfaces } from 'node:os';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter as Router, NavLink, useHistory, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  useHistory,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../state';
 import { RootState } from '../state/reducers';
@@ -8,6 +16,7 @@ import { RootState } from '../state/reducers';
 // page needs text with user name at the top
 // div with create room button and location input text box
 // div with join room button and roomId input text box
+
 const HomePage = () => {
   const [location, setLocation] = useState('');
   const [roomID, setRoomID] = useState('');
@@ -15,98 +24,113 @@ const HomePage = () => {
   const [wrongInfo, setWrongInfo] = useState(false);
 
   const dispatch = useDispatch();
-  const { newRoomID } = bindActionCreators(actionCreators, dispatch)
+  const { newRoomID, newRestList } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
-  const handleLocationSubmit = (event:React.FormEvent) => {
+  const handleLocationSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const locationBody = {location};
+    // const location = { locationBody };
     // checks if user has entered a location
-    if (!locationBody) {
+    if (!location) {
       // if not give error message
-        setMissingInfo(true);
+      setMissingInfo(true);
     } else {
-        console.log('location was entered')
-        // invoke api call file
-        
-        // // get request to api
-        // fetch('/login')
-        // .then((res) => {
-        //     // if the location doesn't exist
-        //     if (!res) {
-        //         // send error message
-        //         setWrongInfo(true);
-        //     } else {
-        //       // const roomID = Math.round(Math.random() * 9999);
-        //       <Redirect to={{
-        //         pathname: "/waiting" + roomID // need to grab from store 
-        //       }}/>
-        //     }
-        // })
-        // .catch(err => console.log('this is err', err));
+      console.log('location was entered');
+      const userLocation = location.replace(' ', '+');
+      //fetch request to google api to gather name, rating, address, photo for restaurant
+      const google_API = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+${userLocation}+united+states&key=AIzaSyC5NRHs0hj0_DyAwHqoTJ0KcGHx_UOstcI`;
+      fetch(google_API)
+        .then((res) => res.json())
+        .then((data) => {
+          const realRestList = [];
+          //iterate through the data that was sent back from google api
+          for (let i = 0; i < data.results.length; i++) {
+            //create an object to store name, rating, address, photo
+            const restaurantList: any = {};
+            restaurantList.name = data.results.name;
+            restaurantList.rating = data.results.rating;
+            restaurantList.address = data.results.formatted_address;
+            restaurantList.photo = `https://maps.googleapis.com/maps/api/place/photo?photoreference=${data.results.photos[0].photo_reference}&sensor=false&maxheight=500&maxwidth=500&key=AIzaSyC5NRHs0hj0_DyAwHqoTJ0KcGHx_UOstcI`;
+            realRestList.push(restaurantList);
+          }
+          newRestList(realRestList);
+        });
     }
-  }
+  };
   // handler for when user clicks the Join Room button
-  const handleRoomIDSubmit = (event:React.FormEvent) => {
+  const handleRoomIDSubmit = (event: React.FormEvent) => {
     event.preventDefault();
 
-    const roomIDBody = {roomID};
+    const roomIDBody = { roomID };
     // checks if user has entered a room id number
     if (!roomIDBody) {
-        // if not give error message
-        setMissingInfo(true);
+      // if not give error message
+      setMissingInfo(true);
     } else {
-        // sends username and password to server 
-        fetch('/login', {
-          method: 'POST',
-          mode: 'cors', 
-          headers: {
-          'Content-Type': 'application/json'              
-          },
-          body: JSON.stringify(roomIDBody)
-        })
+      // sends username and password to server
+      fetch('/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(roomIDBody),
+      })
         .then((res) => {
           // if there is no room with that Id
           if (!res) {
             // give error message
             setWrongInfo(true);
           } else {
-          // otherwise redirect user to waiting room
-          <Redirect to= {{
-            pathname: "/waiting" + roomID
-          }}/>
-        }
-      })
-        .catch(err => console.log('this is err', err));
+            // otherwise redirect user to waiting room
+            <Redirect
+              to={{
+                pathname: '/waiting' + roomID,
+              }}
+            />;
+          }
+        })
+        .catch((err) => console.log('this is err', err));
     }
-  }
+  };
   return (
-    <div className="homePage">
+    <div className='homePage'>
       <h1>Welcome to Rinder </h1>
-      <form onSubmit={handleLocationSubmit}> 
+      <form onSubmit={handleLocationSubmit}>
         <label>
           <p>Enter Location</p>
-          <input type="text" name="locationText" onChange={event => setLocation(event.target.value)}/>
+          <input
+            type='text'
+            name='locationText'
+            onChange={(event) => setLocation(event.target.value)}
+          />
         </label>
-        <div className="locationButton">
-          <button type="submit">Create Room</button>
+        <div className='locationButton'>
+          <button type='submit'>Create Room</button>
         </div>
-        {missingInfo ? <div>Please enter a location</div>:null}
-        {wrongInfo ? <div>Please enter an accurate location</div>:null}
+        {missingInfo ? <div>Please enter a location</div> : null}
+        {wrongInfo ? <div>Please enter an accurate location</div> : null}
       </form>
-      <form onSubmit={handleRoomIDSubmit}> 
+      <form onSubmit={handleRoomIDSubmit}>
         <label>
           <p>Enter the Room ID number</p>
-          <input type="text" name="roomIDText" onChange={event => setRoomID(event.target.value)}/>
+          <input
+            type='text'
+            name='roomIDText'
+            onChange={(event) => setRoomID(event.target.value)}
+          />
         </label>
-        <div className="joinRoomButton">
-          <button type="submit">Join Room</button>
+        <div className='joinRoomButton'>
+          <button type='submit'>Join Room</button>
         </div>
-        {missingInfo ? <div>Please input a Room ID number</div>:null}
-        {wrongInfo ? <div>Please enter an accurate Room ID Number</div>:null}
+        {missingInfo ? <div>Please input a Room ID number</div> : null}
+        {wrongInfo ? <div>Please enter an accurate Room ID Number</div> : null}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,25 +1,27 @@
-import { ActionType } from "../action-types/index"
+import { ActionType } from '../action-types/index';
 
 interface UserIDAction {
-    type: ActionType.USERID,
-    payload: number
+  type: ActionType.USERID;
+  payload: number;
 }
 
 interface RoomIDAction {
-  type: ActionType.ROOMID,
-  payload: string
+  type: ActionType.ROOMID;
+  payload: string;
 }
 
 interface UserListAction {
-  type: ActionType.USERLIST,
-  payload: Array<string>
+  type: ActionType.USERLIST;
+  payload: Array<string>;
 }
 
 interface RestListAction {
-  type: ActionType.RESTLIST,
-  payload: Array<string>
+  type: ActionType.RESTLIST;
+  payload: Array<object>;
 }
 
-
-
-export type Action = UserIDAction | RoomIDAction | UserListAction | RestListAction;
+export type Action =
+  | UserIDAction
+  | RoomIDAction
+  | UserListAction
+  | RestListAction;
