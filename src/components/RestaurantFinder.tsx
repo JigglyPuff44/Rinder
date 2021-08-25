994f0740f68910efbbf30ca56a286668f070b658
farazmoallemi <moallemifaraz@gmail.com>
2021-08-25 10:03:40 -0700

    Adding HTML elements to Restaurant Finder page

--- src/components/RestaurantFinder.tsx
+++ src/components/RestaurantFinder.tsx
@@ -11,10 +11,17 @@ const RestaurantFinder = () => {
   // create 2 buttons for upvoting and downvoting
   // create an h1 tag with the room id number
   // create an h2 tag with the restaurant name
-  // create label for rating(hopefully changed to rating later)
+  // create label for rating(hopefully changed to stars/icons later)
   return (
     <div>
-      
+      <h1>Room ID Number: </h1>
+      <h2>The Chosen Restaurant is: </h2>
+      <div className="swipeLeftButton">
+        <button type="submit">Eww</button>
+      </div>
+      <div className="swipeRightButton">
+        <button type="submit">Yum</button>
+      </div>
     </div>
   )
 }
