7e4c29407dc474943ea555ac9554680b8de6640e
farazmoallemi <moallemifaraz@gmail.com>
2021-08-24 22:50:45 -0700

    planning out restaurant finder

--- src/components/RestaurantFinder.tsx
+++ src/components/RestaurantFinder.tsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, NavLink, useHistory, Route, Switch, Redirect } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../state';
+import { RootState } from '../state/reducers';
 
 
 const RestaurantFinder = () => {
+  // create image linked to api
+  // create 2 buttons for upvoting and downvoting
+  // create an h1 tag with the room id number
+  // create an h2 tag with the restaurant name
+  // create label for rating(hopefully changed to rating later)
   return (
     <div>
       
